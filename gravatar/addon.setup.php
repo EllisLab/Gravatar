3d911292be3d05cb1ca433e96d577f9cccfb2244
Derek Jones <derek.jones@ellislab.com>
2016-02-22 18:34:43 -0800

    consolidating composer dev/func dependencies

--- gravatar/addon.setup.php
+++ gravatar/addon.setup.php
@@ -5,7 +5,7 @@ return array(
 	'author_url'  => 'https://ellislab.com/',
 	'name'        => 'Gravatar',
 	'description' => 'Outputs Globally Recognized Avatars (Gravatars) for a given email address',
-	'version'     => '1.0.0',
+	'version'     => '1.0.1',
 	'namespace'   => 'User\Addons\Gravatar',
 	'settings_exist' => FALSE
 );
